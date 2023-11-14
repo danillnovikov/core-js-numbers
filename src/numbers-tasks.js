d8ef79d2ab783a0961ca3913802ee28bc2abdbaf
Daniil <danill.novikov@mail.ru>
2023-11-14 14:13:50 +0300

    feat: add tasks

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -18,8 +18,8 @@
  *   5, 10 => 50
  *   5, 5  => 25
  */
-function getRectangleArea(/* width, height */) {
-  throw new Error('Not implemented');
+function getRectangleArea(width, height) {
+  return width * height;
 }
 
 /**
@@ -29,12 +29,12 @@ function getRectangleArea(/* width, height */) {
  * @return {number}
  *
  * @example:
- *   5    => 31.41592653589793
- *   3.14 => 19.729201864543903
- *   0    => 0
+ * 5 => 31.41592653589793
+ * 3.14 => 19.729201864543903
+ * 0 => 0
  */
-function getCircleCircumference(/* radius */) {
-  throw new Error('Not implemented');
+function getCircleCircumference(radius) {
+  return Math.PI * 2 * radius;
 }
 
 /**
@@ -45,12 +45,12 @@ function getCircleCircumference(/* radius */) {
  * @return {number}
  *
  * @example:
- *   5, 5  => 5
- *  10, 0  => 5
- *  -3, 3  => 0
+ * 5, 5 => 5
+ * 10, 0 => 5
+ * -3, 3 => 0
  */
-function getAverage(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function getAverage(value1, value2) {
+  return value1 / 2 + value2 / 2;
 }
 
 /**
@@ -64,12 +64,14 @@ function getAverage(/* value1, value2 */) {
  * @return {number}
  *
  * @example:
- *   (0,0) (0,1)    => 1
- *   (0,0) (1,0)    => 1
- *   (-5,0) (10,-10) => 18.027756377319946
+ * (0,0) (0,1) => 1
+ * (0,0) (1,0) => 1
+ * (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  return Math.sqrt(
+    (Math.abs(x1) + Math.abs(x2)) ** 2 + (Math.abs(y2) + Math.abs(y1)) ** 2
+  );
 }
 
 /**
@@ -80,12 +82,12 @@ function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
  * @return {number}
  *
  * @example:
- *   5*x - 10 = 0    => 2
- *   x + 8 = 0       => -8
- *   5*x = 0         => 0
+ * 5*x - 10 = 0 => 2
+ * x + 8 = 0 => -8
+ * 5*x = 0 => 0
  */
-function getLinearEquationRoot(/* a, b */) {
-  throw new Error('Not implemented');
+function getLinearEquationRoot(a, b) {
+  return b / -a;
 }
 
 /**
